69aabb4c9b1aa6fe4488f8d436892d483ca2afad
Felix Mellmann <flix@benfm.de>
2021-12-26 11:27:10 +0100

    Implemented special service UUID which allows to select whether a service shall be advertised or not.

---
+++ main/BleServiceUuid.hpp
@@ -0,0 +1,23 @@
+#ifndef MAIN_BLESERVICEUUID_HPP_
+#define MAIN_BLESERVICEUUID_HPP_
+
+#include "BleUuid.hpp"
+
+namespace Esp32
+{
+
+struct BleServiceUuid: public BleUuid
+{
+public:
+    BleServiceUuid(
+        Width width,
+        uint32_t uuid,
+        bool advertise = true);
+    virtual ~BleServiceUuid();
+
+    bool advertise;
+};
+
+} /* namespace Esp32 */
+
+#endif /* MAIN_BLESERVICEUUID_HPP_ */
