0e0ab0e166e598659eea0c6f9f7930ccdf5ec462
Felix Mellmann <flix@benfm.de>
2021-12-27 09:19:15 +0100

    Made writeCharacteristic() method virtual to allow overriding in sub classes.

--- main/GattsService.hpp
+++ main/GattsService.hpp
@@ -35,7 +35,7 @@ public:
 
     void addCharacteristic(GenericGattCharacteristic* characteristic);
     void readCharacteristic(uint16_t handle, uint8_t* buffer, uint16_t* length);
-    void writeCharacteristic(uint16_t handle, const uint8_t* buffer, uint16_t length);
+    virtual void writeCharacteristic(uint16_t handle, const uint8_t* buffer, uint16_t length);
     void pushHandles(const uint16_t* handles);
     bool hasHandle(uint16_t handle);
 
